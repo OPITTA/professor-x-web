f9000eaf2e74b5dc919ce783addd33cf723e45c8
XinCao <510655387@qq.com>
2016-11-07 17:01:39 +0800

    添加业务dao层

--- src/main/webapp/WEB-INF/resources/doc/table.sql
+++ src/main/webapp/WEB-INF/resources/doc/table.sql
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS `computer` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `cpu_id` int(11) NOT NULL,
     `mem_id` int(11) NOT NULL,
-    'dist_id' int(11) NOT NULL,
-    'solid_dist_id' int(11) NOT NULL,
+    `dist_id` int(11) NOT NULL,
+    `solid_dist_id` int(11) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS `computer` (
 CREATE TABLE IF NOT EXISTS `cpu` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `model` varchar(255) NOT NULL,
-    'frequency' float(11),
+    `frequency` float(11),
     `core_num` int(11) NOT NULL,
-    'describe' varchar(255) NOT NULL,
+    `describe` varchar(255) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `mem` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `model` varchar(255) NOT NULL,
     `capacity` int(11) NOT NULL,
-    'describe' varchar(255) NOT NULL,
+    `describe` varchar(255) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `dist` (
     `model` varchar(255) NOT NULL,
     `capacity` int(11) NOT NULL,
     `speed` int(11) NOT NULL,
-    'describe' varchar(255) NOT NULL,
+    `describe` varchar(255) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -55,33 +55,33 @@ CREATE TABLE IF NOT EXISTS `solid_dist` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `model` varchar(255) NOT NULL,
     `capacity` int(11) NOT NULL,
-    'describe' varchar(255) NOT NULL,
+    `describe` varchar(255) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `report` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    'user_id' int(11) NOT NULL,
-    'computer_id' int(11) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `computer_id` int(11) NOT NULL,
     `title` varchar(255) NOT NULL,
     `describe` text NOT NULL,
     `content` text NOT NULL,
-    'summary' text NOT NULL,
-    'status' int(11) NOT NULL,
+    `summary` text NOT NULL,
+    `status` int(11) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `data` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    'report_id' int(11) NOT NULL,
-    'average_rt' int(11) NOT NULL,
-    'min_rt' int(11) NOT NULL,
-    'max_rt' int(11) NOT NULL,
-    'tps' int(11) NOT NULL,
-    'concurrency' int(11) NOT NULL,
-    'error_rate' int(11) NOT NULL,
+    `report_id` int(11) NOT NULL,
+    `average_rt` int(11) NOT NULL,
+    `min_rt` int(11) NOT NULL,
+    `max_rt` int(11) NOT NULL,
+    `tps` int(11) NOT NULL,
+    `concurrency` int(11) NOT NULL,
+    `error_rate` int(11) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT NOW(),
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
