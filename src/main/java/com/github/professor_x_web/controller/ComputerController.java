53a9011747062531977e6afc626322a8ec048ea2
XinCao <510655387@qq.com>
2016-11-07 19:20:11 +0800

    ok

---
+++ src/main/java/com/github/professor_x_web/controller/ComputerController.java
@@ -0,0 +1,24 @@
+/**
+ * 欢迎浏览和修改代码，有任何想法可以email我
+ */
+package com.github.professor_x_web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author 510655387@qq.com
+ */
+@Controller
+@RequestMapping("/computer/")
+public class ComputerController {
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String listAction(HttpServletRequest httpServletRequest, Model model) {
+        return "/computer/list";
+    }
+}
