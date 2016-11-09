a05ce3f587c6238e7ac8b63183f611928c87f3f4
XinCao <510655387@qq.com>
2016-11-09 14:58:22 +0800

    开始开发报告模块

--- src/main/java/com/github/professor_x_web/controller/ReportController.java
+++ src/main/java/com/github/professor_x_web/controller/ReportController.java
@@ -3,7 +3,13 @@
  */
 package com.github.professor_x_web.controller;
 
+import com.github.professor_x_web.constent.Config;
+import com.github.professor_x_web.model.ReportWithBLOBs;
+import com.github.professor_x_web.service.ReportService;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +23,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/report/")
 public class ReportController {
 
+    @Autowired
+    private ReportService reportService;
+
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public String listAction(HttpServletRequest httpServletRequest, Model model) {
+        HttpSession httpSession = httpServletRequest.getSession();
+        Integer userId = (Integer) httpSession.getAttribute(Config.USER_ID);
+        List<ReportWithBLOBs> reports = reportService.getReportByUserId(userId);
+        model.addAttribute("reports", reports);
         return "/report/list";
     }
 }
